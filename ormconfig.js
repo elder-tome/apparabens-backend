7d550f3ad384f1cc1e21304d36647822fc1da975
elder-tome <r14.elderalves@gmail.com>
2021-09-17 01:55:33 -0300

    ormconfig update

--- ormconfig.js
+++ ormconfig.js
@@ -1,14 +1,16 @@
 module.exports = {
   'type': 'postgres',
   'url': process.env.DATABASE_URL,
-  'ssl': true,
+  // 'ssl': true,
   'synchronize': true,
   'logging': false,
   'entities': [
-    process.env.DEV_ENVIRONMENT === 'true' ? 'src/models/**/*.ts' : 'build/models/**/*.js'
+    // process.env.NODE_ENV === 'production' ? 'build/models/**/*.js' : 'src/models/**/*.ts'
+    'build/models/**/*.js'
   ],
   'migrations': [
-    process.env.DEV_ENVIRONMENT === 'true' ? 'src/migration/**/*.ts' : 'build/migration/**/*.js'
+    // process.env.NODE_ENV === 'production' ? 'build/migration/**/*.js' : 'src/migration/**/*.ts'
+    'build/migration/**/*.js'
   ],
   'cli': {
     'entitiesDir': 'src/models',
